a00b14c07d8275d5ae7da6053b64e7c9ce7e308a
Axel Segundo <axeljasegundo@gmail.com>
2020-09-25 16:50:32 -0500

    63 - 66

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,7 @@ import {APP_ROUTING} from './app.routes';
 // Services
 import { HeroesService } from './servicios/heroes.service';
 
-// Components
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
-import { HeroeComponent } from './components/heroe/heroe.component';
-import { BuscadorComponent } from './components/buscador/buscador.component';
-import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
 
 @NgModule({
   declarations: [
